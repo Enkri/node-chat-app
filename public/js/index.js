2d0e19f246ed0826c853f17cb4550e85e8b5bb47
Chen Lin <Enkri_@macbook-pro.devices.brown.edu>
2017-07-05 21:08:53 -0400

    Scroll to bottom if user is close to buttom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToButtom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+     messages.scrollTop(scrollHeight);
+  }
+};
+
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -17,6 +34,7 @@ socket.on('newMessage', function(message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToButtom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -29,7 +47,7 @@ socket.on('newLocationMessage', function (message) {
     url: message.url
   })
   jQuery('#messages').append(html);
-
+  scrollToButtom();
   // // render with jQuery
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank"> My Current Location </a>');
